9de9b65aa31428c8467d73721e01f28e8555ac57
koros99 <koronje254@gmail.com>
2019-04-07 13:25:50 +0300

    Add function that encrypts letters and words

--- src/main/java/CeaserCypher.java
+++ src/main/java/CeaserCypher.java
@@ -7,8 +7,22 @@ public class CeaserCypher {
           errorMessage = "Enter letters only";
        } else if (key < 1 || key > 25){
            errorMessage = "The key must between 1 and 25";
+       } else {
+           String cypherText = "";
+           int length = encryptionString.length();
+           for (int i = 0; i < length; i++){
+               char ch = encryptionString.charAt(i);
+               char c = (char)(ch + key);
+               if (c > 'z'){
+                   cypherText += (char)(ch - (26-key));
+               } else {
+                   cypherText += c;
+               }
+           }
+           return cypherText;
        }
        return errorMessage;
+
 
    }
 }
