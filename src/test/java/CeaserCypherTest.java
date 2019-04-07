3990862ce8ff2d82379b0a4cbb1a27db6557d670
koros99 <koronje254@gmail.com>
2019-04-07 13:25:12 +0300

    Test if class encrypts letters and words

--- src/test/java/CeaserCypherTest.java
+++ src/test/java/CeaserCypherTest.java
@@ -5,26 +5,32 @@ public class CeaserCypherTest {
     @Test
     public void cypher_checkForNumbers_String(){
         CeaserCypher testCeaserCypher = new CeaserCypher();
-        String expectedOutput = ("Enter letters only");
+        String expectedOutput = "Enter letters only";
         assertEquals(expectedOutput, testCeaserCypher.cypher("Encrypt234", 2));
     }
     @Test
     public void cypher_checkValueOfKey_String(){
         CeaserCypher testCeaserCypher = new CeaserCypher();
-        String expectedOutput = ("The key must between 1 and 25");
+        String expectedOutput = "The key must between 1 and 25";
         assertEquals(expectedOutput, testCeaserCypher.cypher("Encrypt", 27));
     }
     @Test
     public void cypher_checkBothStringAndKey_String(){
         CeaserCypher testCeaserCypher = new CeaserCypher();
-        String expectedOutput = ("Enter letters only, and the key must between 1 and 25");
+        String expectedOutput = "Enter letters only, and the key must between 1 and 25";
         assertEquals(expectedOutput, testCeaserCypher.cypher("Encrypt234", 27));
     }
     @Test
     public void cypher_checkEncryptionOfSingleLetter_String(){
         CeaserCypher testCeaserCypher = new CeaserCypher();
-        String expectedOutput = ("H");
+        String expectedOutput = "H";
         assertEquals(expectedOutput, testCeaserCypher.cypher("G", 1));
+    }
+    @Test
+    public void cypher_checkEncryptionOfAWord_String(){
+        CeaserCypher testCeaserCypher = new CeaserCypher();
+        String expectedOutput = "IFMMP";
+        assertEquals(expectedOutput, testCeaserCypher.cypher("HELLO", 1));
     }
 }
 
